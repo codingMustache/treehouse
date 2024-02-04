df6983daa3b50234c1599bd4714e98f1ee31fd2a
codingMustache <contact@codingMustache.dev>
2024-02-04 12:03:04 -0600

    fixed some changes

--- main.zsh
+++ main.zsh
@@ -3,42 +3,37 @@
 # Run git status and capture the output
 status_output=$(git status --porcelain)
 
-# Initialize an empty array to store untracked files
-untracked_files=()
+# Initialize an empty array to store files to commit
+files_to_commit=()
 
 # Read each line of the status output
 while IFS= read -r line; do
-  # Check if the line starts with "??" indicating an untracked file
-  if [[ $line =~ ^\?\? ]]; then
-    # Extract the file path and add it to the untracked_files array
+  # Check if the line starts with "[AMDR]" indicating a file that has been modified, added, deleted, or renamed
+  if [[ $line =~ ^[AMDR] ]]; then
+    # Extract the file path and add it to the files_to_commit array
     file_path=${line#??}
     remove_whitespace=${file_path//[[:space:]]/}
-    untracked_files+=("$remove_whitespace")
+    files_to_commit+=("$remove_whitespace")
   fi
 done <<< "$status_output"
 
+# Remove duplicates from the files_to_commit array
+files_to_commit=($(echo "${files_to_commit[@]}" | awk -v RS=' ' '!a[$1]++'))
 
-# Get the absolute path of the current directory
-current_dir=$(pwd)
-
-# Loop over the untracked files array
-for file in "${untracked_files[@]}"; do
-
-  # Get the absolute path of the file
-  file_path="${current_dir}/${file}"
-  echo $file_path
+# Loop over the files to commit array
+for file in "${files_to_commit[@]}"; do
   # Add the file to the Git repository
-  git add "$file_path"
+  git add "$file"
 
   # Open the changes in VS Code
-  zed "$file_path"
+  code "$file"
 
   # Prompt for a commit message
   read -rp "Enter a commit message for $file: " commit_message
 
   # Commit the file with the provided message
   git commit -m "$commit_message"
-
 done
 
-
+# Open the working tree
+git status
